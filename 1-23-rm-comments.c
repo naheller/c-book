03665206a606ce8d6285e395b36799b3de66a71b
Nathan Heller <gritty.desk3330@fastmail.com>
2025-12-27 09:36:41 -0800

    1-23 wip

--- 1-23-rm-comments.c
+++ 1-23-rm-comments.c
@@ -3,21 +3,59 @@
 #define MAX_OUT 1000
 
 int main() {
-    int c, i, idx_start_comment;
+    int c, i, idx_start_comment, in_quote, in_char_const;
     char output[MAX_OUT];
 
     i = 0;
     idx_start_comment = -1;
+    in_char_const = 0;
+    in_quote = 0;
 
     while (i < MAX_OUT && (c = getchar()) != EOF) {
-        // Found start of a comment, and we aren't currently inside one already
-        if (i > 0 && c == '*' && output[i-1] == '/' && idx_start_comment == -1) {
+        /**
+         * Found start of a comment,
+         * and we aren't currently inside one already,
+         * and we aren't inside quotes or a char const
+         */
+        if (
+            i > 0
+            && c == '*'
+            && output[i-1] == '/'
+            && idx_start_comment == -1
+            && in_char_const == 0
+            && in_quote == 0
+        ) {
             idx_start_comment = i - 1;
-        // Found end of comment, and we're inside one currently
-        } else if (i > 0 && c == '/' && output[i-1] == '*' && idx_start_comment != -1) {
+            /**
+             * Found end of a comment,
+             * and we're currently inside one,
+             * but we aren't inside quotes or a char const
+             */
+        } else if (
+            i > 0
+            && c == '/'
+            && output[i-1] == '*'
+            && idx_start_comment != -1
+            && in_char_const == 0
+            && in_quote == 0
+        ) {
             i = idx_start_comment;
             idx_start_comment = -1;
         } else {
+            if (c == '\'') {
+                if (in_char_const == 0) {
+                    in_char_const = 1;
+                } else {
+                    in_char_const = 0;
+                }
+            }
+            if (c == '"') {
+                if (in_quote == 0) {
+                    in_quote = 1;
+                } else {
+                    in_quote = 0;
+                }
+            }
             output[i] = c;
             ++i;
         }
